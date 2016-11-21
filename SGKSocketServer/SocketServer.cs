07ef97a251f48e05cfd21c9f7366fbe3d69f60bc
vajarkov <vajarkov@mail.ru>
2016-11-21 19:20:50 +0600

    I dont remember why

--- SGKSocketServer/SocketServer.cs
+++ SGKSocketServer/SocketServer.cs
@@ -19,21 +19,21 @@ namespace SGKSocketServer
 	public class SocketServer
 	{
 		// Incoming data from the client.
-    	public static string data = null;
+		public static byte[] data = null;
 
-    	public static void StartListening(int sum_size, int port) {
+    	public static void StartListening(int port) {
         // Data buffer for incoming data.
-        byte[] bytes = new Byte[sum_size];
+        byte[] bytes = new Byte[sizeof(discreteData) + sizeof(analogData)];
 
         // Establish the local endpoint for the socket.
         // Dns.GetHostName returns the name of the 
         // host running the application.
         IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
         IPAddress ipAddress = ipHostInfo.AddressList[0];
-        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
 
         // Create a TCP/IP socket.
-        Socket listener = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp );
+        Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream,  ProtocolType.Tcp);
 
         // Bind the socket to the local endpoint and 
         // listen for incoming connections.
@@ -46,9 +46,9 @@ namespace SGKSocketServer
                 // Program is suspended while waiting for an incoming connection.
                 Socket handler = listener.Accept();
                 data = null;
-				handler.Send(msg);
-                //handler.Shutdown(SocketShutdown.Both);
-                //handler.Close();
+				handler.Send(data);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
             }
 
         } catch (Exception e) {
